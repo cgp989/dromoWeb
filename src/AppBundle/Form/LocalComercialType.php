cfaf0984fec8107e16576863fc6013286dc16cc2
pcaba <pablonicolascaballero@gmail.com>
2016-06-28 13:43:20 -0300

    Correccion servicio login con suscripciones

--- src/AppBundle/Form/LocalComercialType.php
+++ src/AppBundle/Form/LocalComercialType.php
@@ -19,16 +19,15 @@ class LocalComercialType extends AbstractType {
                 ->add('nombreContacto')
                 ->add('emailContacto')
                 ->add('telefonoContacto')
-        ;
-
-        if (isset($this->opciones['edit']) && $this->opciones['edit']) {
-            $builder
-                    ->add('usuario', null, array(
-                        'label' => 'Tipo',
-                        'disabled' => true,
-                        'empty_value' => '',
-            ));
-        } else {
+;
+//        if (isset($this->opciones['edit']) && $this->opciones['edit']) {
+//            $builder
+//                    ->add('usuario', null, array(
+//                        'label' => 'Tipo',
+//                        'disabled' => true,
+//                        'empty_value' => '',
+//            ));
+//        } else {
 //            $builder->add('usuario', 'entity', array(
 //                    'class' => 'AppBundle:Usuario',
 //                    'query_builder' =>
@@ -40,24 +39,23 @@ class LocalComercialType extends AbstractType {
 //                    'label' => 'Usuario'
 //                        )
 //                );
-        }
-            }
+        //  }
+    }
 
-            /**
-             * @param OptionsResolverInterface $resolver
-             */
-            public function setDefaultOptions(OptionsResolverInterface $resolver) {
-                $resolver->setDefaults(array(
-                    'data_class' => 'AppBundle\Entity\LocalComercial'
-                ));
-            }
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\LocalComercial'
+        ));
+    }
 
-            /**
-             * @return string
-             */
-            public function getName() {
-                return 'appbundle_localcomercial';
-            }
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'appbundle_localcomercial';
+    }
 
-        }
-        
+}
