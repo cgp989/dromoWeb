447ae4d51c67b396b566526fc454fcaef531f91c
pcaba <pablonicolascaballero@gmail.com>
2016-07-14 20:55:37 -0300

    USW74 terminada

--- src/AppBundle/Controller/CobrosController.php
+++ src/AppBundle/Controller/CobrosController.php
@@ -10,6 +10,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\EstadoCobroCupon;
 
 /**
  * Description of CobrosController
@@ -17,30 +18,54 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * @author CRISTIANGILBERTO
  */
 class CobrosController extends Controller {
+
     /**
      * Lista los locales que tienen cobros pendientes
      */
-    public function showLocalesPendientesAction()
-    {
+    public function showLocalesPendientesAction() {
         $repositoryLocal = $this->getDoctrine()->getRepository('AppBundle:LocalComercial');
         $locales = $repositoryLocal->getPendientesCobro();
-        
+
         return $this->render('AppBundle:Cobros:showLocalesPendientes.html.twig', array(
                     'locales' => $locales,
         ));
     }
-    
+
     /**
      * Me retorna un listado de los cupones pendientes de cobro de un local
      * 
      * @param type $idLocal
      */
-    public function getPendientesLocalAction($id){
+    public function getPendientesLocalAction($id) {
         //PABLO ACA TENES QUE HACER LA CONSULTA QUE TRAIGA LOS CUPONES PENDIENTES DE UN LOCAL. 
         //HACELO ADENTRO DEL REPOSITORIO DE CUPON O DE LOCAL
-        $cuponesPendientes = ''; 
+        $repositoryLocal = $this->getDoctrine()->getRepository('AppBundle:LocalComercial');
+        $cuponesPendientes = $repositoryLocal->getItemsPendientesCobro($id);
         return $this->render('AppBundle:Cobros:listarPendientesLocal.html.twig', array(
                     'cuponesPendientes' => $cuponesPendientes,
         ));
     }
+
+    /**
+     * Setea estado "cobrado" de cupon y recarga pagina de pendientes de cobrar
+     * 
+     * @param type $id
+     * @param type $idCupon
+     */
+    public function setCobroLocalAction($id, $idCupon) {
+        //setear estado "cobrado"
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('AppBundle:Cupon')->find($idCupon);
+        $estadoCobrado = $em->getRepository('AppBundle:EstadoCobroCupon')->findOneByNombre('cobrado');
+        $entity->setEstadoCobroCupon($estadoCobrado);
+        $em->persist($entity);
+        $em->flush();
+        //redirigir
+        $repositoryLocal = $this->getDoctrine()->getRepository('AppBundle:LocalComercial');
+        $cuponesPendientes = $repositoryLocal->getItemsPendientesCobro($id);
+        return $this->render('AppBundle:Cobros:listarPendientesLocal.html.twig', array(
+                    'cuponesPendientes' => $cuponesPendientes,
+        ));
+    }
+
 }
