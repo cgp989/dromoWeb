aa698037cb49d695de546baf4e78c5005a1cdf58
pcaba <pablonicolascaballero@gmail.com>
2016-07-26 17:04:39 -0300

    decodifica comentarios

--- src/Dromo/Bundle/ApiPromocionesBundle/Controller/ComentariosRestController.php
+++ src/Dromo/Bundle/ApiPromocionesBundle/Controller/ComentariosRestController.php
@@ -74,7 +74,8 @@ class ComentariosRestController extends Controller {
             /* @var $comen Entity\Comentario */
             $comen = new Entity\Comentario();
             $em = $this->getDoctrine()->getManager();
-
+            $comenDecode = urldecode($comentario);
+            $comentario = str_replace('€', '.', $comenDecode);
             $comen->setComentario($comentario);
             $comen->setValoracion($valoracion);
             $comen->setLocalComercial($localComercial);
@@ -86,20 +87,20 @@ class ComentariosRestController extends Controller {
             $em->persist($comen);
             $em->flush();
             $comentariosLocal = $this->getDoctrine()->getRepository('AppBundle:Comentario')->findByLocalComercial($localComercial);
-            $suma=0;
-            $cant=0;
-            foreach ($comentariosLocal as $coment) {                
-                 $cant++;
-                 $suma+= $coment->getValoracion();
-             }
-             if($cant==0){
-                  $localComercial->setValoracion(0);
-                 //$em->persist($localComercial);
-             }else{
-                $localComercial->setValoracion((double)$suma/$cant); 
+            $suma = 0;
+            $cant = 0;
+            foreach ($comentariosLocal as $coment) {
+                $cant++;
+                $suma+= $coment->getValoracion();
+            }
+            if ($cant == 0) {
+                $localComercial->setValoracion(0);
                 //$em->persist($localComercial);
-             }
-            
+            } else {
+                $localComercial->setValoracion((double) $suma / $cant);
+                //$em->persist($localComercial);
+            }
+
             $em->flush();
         }
         if (isset($error)) {
